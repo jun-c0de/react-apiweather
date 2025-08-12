e88f65d98c5a28f3d696caad63be7c7600e798b2
jun-c0de <sh48520414@gmail.com>
2025-08-12 12:20:36 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import WeatherCard from './components/WeatherCard'
 import { useState, useRef, useEffect } from 'react'
 import { fetchCoordinates } from './api/geo'
 import { fetchWeatherByCoords } from './api/weather'
+import { getColorByWeatherId } from './api/bgColor'
+
 function App() {
 
   const [city, setCity] = useState('seoul')
@@ -49,25 +51,33 @@ function App() {
     if (e.key === 'Enter') handleSearch()
   }
 
+  const bg = weather?.weather?.[0]?.id
+    ? getColorByWeatherId(weather.weather[0].id)
+    : 'linear-gradient(135deg, #FFFFFF 0%, #F1F5F9 100%)';
+
   return (
-    <div className='app'>
-      <h1>성준의 날씨앱</h1>
-      <div className="input-wrap">
-        <input
-          ref={inputRef}
-          value={city}
-          onChange={onChangeInput}
-          onKeyUp={onKeyup}
-          type="text"
-          placeholder='도시이름을 입력하세요' />
-        <button onClick={handleSearch} disabled={loading}>
-          {loading ? "검색중...." : "검색"}
-        </button>
+    <section style={{ background: bg, minHeight: '100vh', transition: 'background .3s ease' }}>
+      <div className='app' >
+        <div className='app'>
+          <h1>성준의 날씨앱</h1>
+          <div className="input-wrap">
+            <input
+              ref={inputRef}
+              value={city}
+              onChange={onChangeInput}
+              onKeyUp={onKeyup}
+              type="text"
+              placeholder='도시이름을 입력하세요' />
+            <button onClick={handleSearch} disabled={loading}>
+              {loading ? "검색중...." : "검색"}
+            </button>
+          </div>
+          {err && <p className='error'>{err}</p>}
+          {loading && <p className='info'>불러오는중...</p>}
+          <WeatherCard weather={weather} />
+        </div>
       </div>
-      {err && <p className='error'>{err}</p>}
-      {loading && <p className='info'>불러오는중...</p>}
-      <WeatherCard weather={weather} />
-    </div>
+    </section>
   )
 }
 
